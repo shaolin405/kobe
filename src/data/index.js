e9cbc3018dcc45cedea8969ad3f465d6205dfb7b
Lei.Xing <lei.xing@hotmail.fr>
2019-02-23 17:36:19 +0100

    Data setup :memo:

---
+++ src/data/index.js
@@ -0,0 +1,222 @@
+export default {
+  seasons: {
+    '1996-1997': {
+      title: '13th Draft Pick',
+      videoUrl: 'https://www.youtube.com/watch?v=eaJMLnt5bdU',
+      stats: {
+        pts: 7.6,
+        ast: 1.3,
+        rbd: 1.9,
+        stl: 0.7,
+        mpg: 15.5
+      },
+      teammates: {}
+    },
+    '1997-1998': {
+      title: 'The "Kid" - Youngest All-Star',
+      videoUrl: 'https://www.youtube.com/watch?v=1x3cOWs0bRM',
+      stats: {
+        pts: 15.4,
+        ast: 2.5,
+        rbd: 3.1,
+        stl: 0.9,
+        mpg: 26.0
+      }
+    },
+    '1998-1999': {
+      title: 'Full-time Starter',
+      videoUrl: 'https://www.youtube.com/watch?v=Dh_dWLMHGYs',
+      stats: {
+        pts: 19.9,
+        ast: 3.8,
+        rbd: 5.3,
+        stl: 1.4,
+        mpg: 37.9
+      }
+    },
+    '1999-2000': {
+      title: 'Phil Jackson - 1st Ring',
+      videoUrl: 'https://www.youtube.com/watch?v=z_ZGtDL7mPI',
+      stats: {
+        pts: 22.5,
+        ast: 4.9,
+        rbd: 6.3,
+        stl: 1.6,
+        mpg: 38.2
+      }
+    },
+    '2000-2001': {
+      title: 'Back-to-back title',
+      videoUrl: 'https://www.youtube.com/watch?v=sS6hlq1OvqU',
+      stats: {
+        pts: 28.5,
+        ast: 5.0,
+        rbd: 5.9,
+        stl: 1.7,
+        mpg: 40.9
+      }
+    },
+    '2001-2002': {
+      title: '3-Peat',
+      videoUrl: 'https://www.youtube.com/watch?v=nxmlAZqwQ9g',
+      stats: {
+        pts: 25.2,
+        ast: 5.5,
+        rbd: 5.5,
+        stl: 1.5,
+        mpg: 38.1
+      }
+    },
+    '2002-2003': {
+      title: '9 straight 40+ games',
+      videoUrl: 'https://www.youtube.com/watch?v=D8eqg-tGb3A',
+      stats: {
+        pts: 30.0,
+        ast: 5.0,
+        rbd: 5.9,
+        stl: 2.2,
+        mpg: 41.5
+      }
+    },
+    '2003-2004': {
+      title: 'NBA Finals disappointment',
+      videoUrl: 'https://www.youtube.com/watch?v=DUfAQ2iqG8Q',
+      stats: {
+        pts: 24.0,
+        ast: 5.1,
+        rbd: 5.5,
+        stl: 1.7,
+        mpg: 37.6
+      }
+    },
+    '2004-2005': {
+      title: 'Kobe-Shaq break-up',
+      videoUrl: 'https://www.youtube.com/watch?v=fS4EKSY3MDs',
+      stats: {
+        pts: 27.6,
+        ast: 6.0,
+        rbd: 5.9,
+        stl: 1.3,
+        mpg: 40.7
+      }
+    },
+    '2005-2006': {
+      title: 'Eighty-one',
+      videoUrl: 'https://www.youtube.com/watch?v=fS4EKSY3MDs',
+      stats: {
+        pts: 35.4,
+        ast: 4.5,
+        rbd: 5.3,
+        stl: 1.8,
+        mpg: 41
+      }
+    },
+    '2006-2007': {
+      title: '8 to 24',
+      videoUrl: 'https://www.youtube.com/watch?v=1wbq2rErc6I',
+      stats: {
+        pts: 31.6,
+        ast: 5.4,
+        rbd: 5.7,
+        stl: 1.4,
+        mpg: 40.8
+      }
+    },
+    '2007-2008': {
+      title: 'MVP & Gold Medal',
+      videoUrl: 'https://www.youtube.com/watch?v=XnErp0jf0zc',
+      stats: {
+        pts: 28.3,
+        ast: 5.4,
+        rbd: 6.3,
+        stl: 1.8,
+        mpg: 38.9
+      }
+    },
+    '2008-2009': {
+      title: '4th Ring',
+      videoUrl: 'https://www.youtube.com/watch?v=M7pEsNGvUmc',
+      stats: {
+        pts: 26.8,
+        ast: 4.9,
+        rbd: 5.2,
+        stl: 1.5,
+        mpg: 36.1
+      }
+    },
+    '2009-2010': {
+      title: 'Number five',
+      videoUrl: 'https://www.youtube.com/watch?v=Si3u8onVXxU',
+      stats: {
+        pts: 27.0,
+        ast: 5.0,
+        rbd: 5.4,
+        stl: 1.5,
+        mpg: 38.3
+      }
+    },
+    '2010-2011': {
+      title: '4th All-Star MVP',
+      videoUrl: 'https://www.youtube.com/watch?v=hCbAakCKa9Y',
+      stats: {
+        pts: 25.3,
+        ast: 4.7,
+        rbd: 5.1,
+        stl: 1.2,
+        mpg: 33.9
+      }
+    },
+    '2011-2012': {
+      title: '2nd gold medal',
+      videoUrl: 'https://www.youtube.com/watch?v=6FMvv4OAHiQ',
+      stats: {
+        pts: 27.9,
+        ast: 4.6,
+        rbd: 5.4,
+        stl: 1.2,
+        mpg: 38.5
+      }
+    },
+    '2012-2013': {
+      title: 'Achilles',
+      videoUrl: 'https://www.youtube.com/watch?v=P674MK3AwSk',
+      stats: {
+        pts: 27.3,
+        ast: 6.0,
+        rbd: 5.6,
+        stl: 1.4,
+        mpg: 38.6
+      }
+    },
+    '2013-2014': {
+      title: 'Injuries',
+      stats: {
+        pts: 13.8,
+        ast: 6.3,
+        rbd: 4.3,
+        stl: 1.2,
+        mpg: 29.5
+      }
+    },
+    '2014-2015': {
+      title: '3rd All-time scoring list',
+      stats: {
+        pts: 22.3,
+        ast: 5.6,
+        rbd: 5.7,
+        stl: 1.3,
+        mpg: 34.5
+      }
+    },
+    '2015-2016': {
+      title: 'Farewell Tour',
+      stats: {
+        pts: 17.6,
+        ast: 2.8,
+        rbd: 3.7,
+        stl: 0.9,
+        mpg: 28.2
+      }
+    }
+  }
+}
